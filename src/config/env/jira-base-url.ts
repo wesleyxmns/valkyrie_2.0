1e18c9171ce6546ecef0c19ac49baf2bb6cf2000
Wesley Ximenes <wximenes60@gmail.com>
2024-11-11 16:16:10 -0300

    chore: Variável ambiente trocada de Jira para BRYNHILDR que é backend da aplicação.

--- src/config/env/jira-base-url.ts
+++ src/config/env/jira-base-url.ts
@@ -1,1 +1,1 @@
-export const JIRA_BASE_URL = `${process.env.NEXT_PUBLIC_JIRA_API}:${process.env.NEXT_PUBLIC_JIRA_API_PORT}`;
+export const JIRA_BASE_URL = `${process.env.NEXT_PUBLIC_BRYNHILDR_API}:${process.env.NEXT_PUBLIC_BRYNHILDR_API_PORT}`;
