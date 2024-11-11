e63648380a0faedf66d92fffb7c6dfc8e7344d5e
Wesley Ximenes <wximenes60@gmail.com>
2024-11-11 16:11:56 -0300

    chore: Adicionada nova validação de rotas públicas ao middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,15 +8,23 @@ export function middleware(request: NextRequest) {
   const tokenCookie = request.cookies.get(COOKIES.AUTH_TOKEN);
   const token = tokenCookie ? tokenCookie.value : null;
 
-  // Verifica se a rota faz parte das rotas de autenticação
-  const isAuthRoute = pathname.startsWith(ROUTES_VARIABLES.AUTH);
+  // Defina uma lista de rotas públicas, que devem estar acessíveis sem autenticação
+  const publicRoutes = [
+    ROUTES_VARIABLES.AUTH, // A página de login
+    ROUTES_VARIABLES.FORGOT_PASSWORD, // A página de recuperação de senha
+    ROUTES_VARIABLES.RESET_PASWORD,  // A página de redefinição de senha, se existir
+    ROUTES_VARIABLES.EXPIRES_LINK,    // A página de link expirado, se existir
+  ];
 
-  // Se o usuário não está autenticado e não está em uma rota de autenticação, redireciona para o login
-  if (!token && !isAuthRoute) {
+  // Verifique se a rota atual é uma das rotas públicas
+  const isPublicRoute = publicRoutes.some(route => pathname.startsWith(route));
+
+  // Se o usuário não está autenticado e a rota não é pública, redirecione para a página de login
+  if (!token && !isPublicRoute) {
     return NextResponse.redirect(new URL(`${ROUTES_VARIABLES.AUTH}`, request.url));
   }
 
-  // Se o usuário autenticado tenta acessar a página de login, redireciona para o dashboard
+  // Se o usuário está autenticado e tenta acessar a rota de login, redirecione para o dashboard
   if (token && pathname === ROUTES_VARIABLES.AUTH) {
     return NextResponse.redirect(new URL(`${ROUTES_VARIABLES.DASHBOARD}`, request.url));
   }
@@ -24,7 +32,7 @@ export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
-// Configuração do matcher para incluir as rotas de autenticação
+// Configuração do matcher para incluir as rotas de autenticação e ignorar rotas estáticas e public
 export const config = {
   matcher: [
     '/((?!_next/static|_next/image|favicon.ico|public).*)', // Intercepta todas as rotas exceto as estáticas e a pasta public
