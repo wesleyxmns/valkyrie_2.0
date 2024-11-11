95a491912da30d286a7943b4ad42aefca716d7ab
Wesley Ximenes <wximenes60@gmail.com>
2024-11-11 16:18:37 -0300

    style: Foram definidos novos estilos no componente de autenticação da aplicação.

--- src/components/features/auth/login-screen.tsx
+++ src/components/features/auth/login-screen.tsx
@@ -7,16 +7,17 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ShinyButton } from "@/components/ui/shiny-button";
 import BlurIn from "@/components/ui/text-blur-in";
-import { ROUTES_VARIABLES } from "@/config/constants/route-variables";
 import { AuthContext } from "@/contexts/auth/auth-context";
 import { UserAuthentication } from "@/shared/interfaces/auth";
 import Image from 'next/image';
 import Link from 'next/link';
 import { useContext, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
-import { AiFillEye, AiFillEyeInvisible, AiOutlineKey, AiOutlineUser } from 'react-icons/ai';
+import { AiOutlineKey, AiOutlineUser } from 'react-icons/ai';
 import { LiaExternalLinkAltSolid } from 'react-icons/lia';
+import { PiEye, PiEyeClosed } from "react-icons/pi";
 import Logo from '../../../../public/assets/images/png/logo-sangati.png';
+import ValkyrieLogo from '../../../../public/svg/valkyrie-logo-icon.svg'
 
 export function LoginScreen() {
   const { signIn, keepMeLoggedIn, setKeepMeLoggedIn } = useContext(AuthContext);
@@ -33,8 +34,8 @@ export function LoginScreen() {
   }
 
   return (
-    <Card className="w-full h-[600px] max-w-4xl flex overflow-hidden relative">
-      <div className="flex-1 flex justify-center flex-col items-center">
+    <Card className="w-full  h-[600px] max-w-4xl flex overflow-hidden relative bg-LoginBackground border-white border">
+      <div className="flex-1 flex w-full justify-center flex-col items-center">
         <CardHeader>
           <CardHeader className='flex items-center justify-center space-y-5'>
             <div className='mb-5'>
@@ -42,21 +43,27 @@ export function LoginScreen() {
             </div>
           </CardHeader>
         </CardHeader>
-        <CardContent className="space-y-4">
+        <CardContent className="space-y-4 p-2">
           <form onSubmit={handleSubmit(login as SubmitHandler<FieldValues>)} className='space-y-5 flex flex-col items-center justify-center'>
-
-            <Input autoComplete='username' {...register('username')} name="username" icon={AiOutlineUser} placeholder="Usuário" />
+            <Input
+              autoComplete='username'
+              {...register('username')}
+              name="username"
+              icon={AiOutlineUser}
+              placeholder="Usuário"
+              className="focus:border-indigo-800"
+            />
             <div className='relative' >
-              <Input autoComplete='password' {...register('password')} name="password" icon={AiOutlineKey} type={showPassword ? 'text' : 'password'} placeholder="Senha" />
+              <Input autoComplete='password' {...register('password')} name="password" icon={AiOutlineKey} type={showPassword ? 'text' : 'password'} placeholder="Senha" className="focus:border-indigo-800" />
               <button type="button" className="absolute top-1/2 transform -translate-y-1/2 right-2 focus:outline-none"
                 onClick={handleTogglePasswordVisibility}
               >
-                {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+                {showPassword ? <PiEye /> : <PiEyeClosed />}
               </button>
             </div>
 
-            <Button variant='link'>
-              <Link href={ROUTES_VARIABLES.FORGOT_PASSWORD} className='cursor-pointer font-normal'>
+            <Button variant='link' asChild>
+              <Link href="/forgot-password" className='cursor-pointer font-normal'>
                 Esqueceu a senha?
               </Link>
             </Button>
@@ -66,7 +73,7 @@ export function LoginScreen() {
                 checked={keepMeLoggedIn}
                 onCheckedChange={(e: boolean) => setKeepMeLoggedIn(e as boolean)}
               />
-              <Label className='font-normal' >Mantenha-me conectado</Label>
+              <Label className='font-normal'>Mantenha-me conectado</Label>
             </div>
 
             <ShinyButton >
@@ -81,13 +88,16 @@ export function LoginScreen() {
           </form>
         </CardContent>
       </div>
-      <div className="flex-1 justify-center items-center w-full bg-gradient-to-br from-slate-950 via-blue-600 to-slate-700 hidden md:flex">
+      <div className="flex-1 justify-center items-center w-full bg-gradient-to-br from-gray-800 via-indigo-800 to-gray-800 hidden md:flex">
         <BlurIn
-          word=".valkyrie"
-          className="text-sm font-bold text-white dark:text-white"
+          content={ValkyrieLogo}
+          isImage
+          imageWidth={300}
+          imageHeight={50}
+          className="text-sm font-bold text-white dark:text-white select-none"
         />
       </div>
-      <BorderBeam size={300} colorFrom="#FEBE10" colorTo="#00529F" />
+      <BorderBeam borderWidth={2} size={300} colorFrom="indigo" colorTo="indigo" />
     </Card>
   );
 }
