c2461dfd061331c3a193474d9fa460b2610a9eae
Wesley Ximenes <wximenes60@gmail.com>
2024-11-11 07:44:49 -0300

    chore: Mapeamento de rotas e chaves globais da aplicação.

---
+++ src/config/constants/cookies.ts
@@ -0,0 +1,5 @@
+export enum COOKIES {
+    AUTH_TOKEN = '@valkyrie:auth-token',
+    KEEP_ME_LOGGED_IN = '@valkyrie:keep-me-logged-in',
+    RECOVERY_PASSWORD_TOKEN = '@recovery-password'
+}
---
+++ src/config/constants/route-variables.ts
@@ -0,0 +1,13 @@
+export const ROUTES_VARIABLES = {
+    HOME: '/',
+    AUTH: '/auth',
+    DASHBOARD: '/dashboard',
+    DATASHEET: '/ficha-tecnica',
+    STRUCTURES: '/estruturas',
+    FORGOT_PASSWORD: '/forgot-password',
+    RESET_PASWORD: '/reset-password',
+    EXPIRES_LINK: '/expires-link',
+    CONSULTBYOP: '/estruturas/consult-by-op',
+    CONSULTBYPRODUCT: '/estruturas/consult-by-product',
+    GETDATASHEETSFILES: '/estruturas/get-datasheets-files',
+}
