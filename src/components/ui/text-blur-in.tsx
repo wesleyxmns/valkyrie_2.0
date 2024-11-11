ed4d6bfb5305aa4b669466421a7e213bf51106b7
Wesley Ximenes <wximenes60@gmail.com>
2024-11-11 16:16:34 -0300

    chore: Adicionado efeito no componente agora a imagens também, caso seja passada

--- src/components/ui/text-blur-in.tsx
+++ src/components/ui/text-blur-in.tsx
@@ -1,19 +1,29 @@
-"use client";
-
 import { cn } from "@/lib/utils/utils";
 import { motion } from "framer-motion";
+import Image, { StaticImageData } from "next/image";
 
-
-interface BlurIntProps {
-  word: string;
+interface BlurInProps {
+  content: string | StaticImageData;
   className?: string;
   variant?: {
     hidden: { filter: string; opacity: number };
     visible: { filter: string; opacity: number };
   };
   duration?: number;
+  isImage?: boolean;
+  imageWidth?: number;
+  imageHeight?: number;
 }
-const BlurIn = ({ word, className, variant, duration = 1 }: BlurIntProps) => {
+
+const BlurIn = ({
+  content,
+  className,
+  variant,
+  duration = 1,
+  isImage = false,
+  imageWidth,
+  imageHeight,
+}: BlurInProps) => {
   const defaultVariants = {
     hidden: { filter: "blur(10px)", opacity: 0 },
     visible: { filter: "blur(0px)", opacity: 1 },
@@ -21,18 +31,32 @@ const BlurIn = ({ word, className, variant, duration = 1 }: BlurIntProps) => {
   const combinedVariants = variant || defaultVariants;
 
   return (
-    <motion.h1
+    <motion.div
       initial="hidden"
       animate="visible"
       transition={{ duration }}
       variants={combinedVariants}
-      className={cn(
-        className,
-        "font-display text-center text-4xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-7xl md:leading-[5rem]",
-      )}
+      className={cn(className, "relative overflow-hidden")}
     >
-      {word}
-    </motion.h1>
+      {isImage ? (
+        <Image
+          src={content as StaticImageData}
+          alt=""
+          className="object-cover"
+          width={imageWidth}
+          height={imageHeight}
+        />
+      ) : (
+        <h1
+          className={cn(
+            "font-display text-center text-4xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-7xl md:leading-[5rem]",
+            className
+          )}
+        >
+          {typeof content === "string" ? content : ""}
+        </h1>
+      )}
+    </motion.div>
   );
 };
 
