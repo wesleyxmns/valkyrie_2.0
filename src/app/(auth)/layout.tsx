a1c0000e11a5824fc4cb4cf4344bb3246ea6eac4
Wesley Ximenes <wximenes60@gmail.com>
2024-11-11 08:32:27 -0300

    chore: Chamado componente Navbar no layout global da aplicação.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,9 +1,11 @@
+import { Navbar } from "@/components/common/nav-bar"
 import { ReactNode } from "react"
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="p-5">
-      {children}
+    <div>
+      <Navbar />
+      <div className="p-5">{children}</div>
     </div>
   )
 }
