1a791d5c1cb1e14374d41a9466657ac42c090f74
Wesley Ximenes <wximenes60@gmail.com>
2024-11-12 08:50:40 -0300

    chore: Passada imagem de background no layout de rotas autenticadas.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,19 @@
 import { Navbar } from "@/components/common/nav-bar"
 import { ReactNode } from "react"
+import AuthRouteBackground from '../../../public/assets/images/jpeg/background-route-auth.jpg'
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
+    <div
+      className="relative w-screen h-screen flex bg-cover bg-center"
+      style={{ backgroundImage: `url(${AuthRouteBackground.src})` }}
+    >
       <Navbar />
-      <div className="p-5">{children}</div>
+      <div className="p-5">
+        {children}
+      </div>
     </div>
   )
 }
-export default AuthLayout
+
+export default AuthLayout;
