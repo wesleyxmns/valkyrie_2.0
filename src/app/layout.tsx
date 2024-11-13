8c6d105c8d9d4ede693562b4a7aee110f77f4b27
Wesley Ximenes <wximenes60@gmail.com>
2024-11-13 09:54:12 -0300

    chore: Passado provider do tanstack para toda a aplicação.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Inter } from 'next/font/google'
 import { Toaster } from 'sonner'
 import { twMerge } from 'tailwind-merge'
@@ -6,6 +7,7 @@ import '../app/styles/globals.css'
 import { AuthProvider } from '@/providers/auth/auth-provider'
 import ThemeProvider from '@/providers/theme/theme-provider'
 import { TooltipProvider } from '@/components/ui/tooltip'
+import TanstackProvider from '@/providers/tanstack-query/tanstack-provider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,23 +20,28 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={twMerge(
-          'w-screen h-screen overflow-hidden',
-          inter.className,
-        )}
-      >
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <AuthProvider>
-            <TooltipProvider>
-              <main>{children}</main>
-              <Toaster richColors />
-            </TooltipProvider>
-          </AuthProvider>
-        </ThemeProvider>
-      </body>
-    </html>
+    <TanstackProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={twMerge(
+            'w-screen h-screen overflow-hidden',
+            inter.className,
+          )}
+        >
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <AuthProvider>
+              <TooltipProvider>
+                <main>
+                  {children}
+                </main>
+                <Toaster richColors />
+              </TooltipProvider>
+            </AuthProvider>
+          </ThemeProvider>
+        </body>
+      </html >
+    </TanstackProvider>
   )
 }
