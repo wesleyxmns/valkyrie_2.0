c20248bd2121142ee1c4d731370168483d3550e2
Wesley Ximenes <wximenes60@gmail.com>
2024-11-13 08:59:24 -0300

    chore: Mapeados novos enums relacionados ao Jira.

---
+++ src/shared/enums/jira-enums/jira-statuses.ts
@@ -0,0 +1,14 @@
+export enum JiraStatuses {
+  BACKLOG = "Backlog",
+  TO_DO = "To Do",
+  EM_ANALISE = "Em Análise",
+  VALIDA = "VALIDA",
+  INVALIDA = "INVALIDA",
+  AGUARDANDO_APROVACAO = "Aguardando Aprovação",
+  APROVADA = "APROVADA",
+  IN_PROGRESS = "In Progress",
+  UNDER_REVIEW = "Under Review",
+  DONE = "Done",
+  REOPENED = "Reopened",
+  CANCELLED = "Cancelled",
+}
