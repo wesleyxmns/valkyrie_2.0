8579a46228ad864af63e7d18b454a3a17fa5da7f
Wesley Ximenes <wximenes60@gmail.com>
2024-11-13 09:24:01 -0300

    chore: Classe de execução de JQL's para busca de tarefas renderizadas no kanban e sua função de filtro de colunas por status.

--- src/lib/utils/utils.ts
+++ src/lib/utils/utils.ts
@@ -4,3 +4,27 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function extractStatusesFromJQL(jql: string): string[] {
+  const statusRegex = /status\s+in\s+\((.*?)\)/g;
+  const statusSingleRegex = /status\s*=\s*["']([^"']+)["']/g;
+  const statuses: string[] = [];
+  let match;
+
+  while ((match = statusRegex.exec(jql)) !== null) {
+    if (match[1]) {
+      const matchedStatuses = match[1]
+        .split(',')
+        .map(status => status.trim().replace(/[']/g, '').toUpperCase());
+      statuses.push(...matchedStatuses);
+    }
+  }
+
+  while ((match = statusSingleRegex.exec(jql)) !== null) {
+    if (match[1]) {
+      statuses.push(match[1].toUpperCase());
+    }
+  }
+
+  return Array.from(new Set(statuses));
+}
