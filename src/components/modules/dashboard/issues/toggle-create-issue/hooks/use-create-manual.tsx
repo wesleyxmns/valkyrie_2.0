49f8fad90f89779c58e8a1f003627e8d01a7c62e
Wesley Ximenes <wximenes60@gmail.com>
2024-11-19 14:46:30 -0300

    chore: Refeita a criação manual do relatório de não conformidade para a implementação do react-query na mesma, mantendo a manutenabilidade, se livrando de request e renderizações desnecessárias.

--- src/components/modules/dashboard/issues/toggle-create-issue/hooks/use-create-manual.tsx
+++ src/components/modules/dashboard/issues/toggle-create-issue/hooks/use-create-manual.tsx
@@ -4,30 +4,36 @@ import { useBuildForm } from '@/components/dynamic-form/hooks/use-build-form';
 import { useActions } from '@/hooks/actions/use-actions';
 import { useAuth } from '@/hooks/auth/use-auth';
 import { useKanban } from '@/hooks/kanban/use-kanban';
-import { brynhildrAPI } from '@/lib/fetch/brynhildr-api';
 import { HttpStatus } from '@/lib/fetch/constants/http-status';
+import { BrynhildrService } from '@/services/external/brynhildr -service/brynhildr -service';
 import { CustomFields } from '@/shared/constants/jira/jira-custom-fields';
-import { JiraCategories } from '@/shared/enums/jira-enums/jira-categories';
 import { JiraStatusesId } from '@/shared/enums/jira-enums/jira-statuses-id';
 import { RNCEpicTransitionsId } from '@/shared/enums/rnc-enums/rnc-epic-transitions-id';
 import { filterFieldsValues } from '@/shared/functions/filter-fields-values';
+import { generateEpicName } from '@/shared/functions/generate-epic-name';
+import { RequestBodys } from '@/shared/functions/requests-body';
 import { TransitionProps } from '@/shared/types/transitions';
+import { useMutation } from '@tanstack/react-query';
 import { parseCookies } from 'nookies';
 import { useCallback, useMemo, useState } from 'react';
 import { FieldValues, SubmitHandler } from 'react-hook-form';
 import { toast } from 'sonner';
 import { useCreateTaskByReport } from './use-create-task-by-report';
-import { useBrynhildrData } from '@/hooks/brynhildr-data/brynhildr-data';
+
+const brynhildrService = new BrynhildrService();
 
 const useCreateManual = (projectKey: string) => {
-  const { useDoTransition } = useBrynhildrData();
+  const { createIssue, getIssue, getTransitions, doTransition, sendAttachments } = brynhildrService
+
   const { user } = useAuth();
   const { '@valkyrie:auth-token': token } = parseCookies();
   const userAuthorization = `Basic ${token}`;
+
   const { form, fieldsComponents } = useBuildForm({ projectKey });
   const { createQualityReport } = useCreateTaskByReport();
   const { actions, resetActions } = useActions();
   const { columns, updateColumns } = useKanban();
+  const { epicBody } = RequestBodys;
 
   const groups = user?.getGroups().items.map((group: any) => group.name);
   const isQualityMember = groups?.find((group) => group.includes("QUALIDADE - Global"))
@@ -36,26 +42,21 @@ const useCreateManual = (projectKey: string) => {
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
   async function qualityIssueInTodo(issueKey: string) {
-    const newIssueResponse = await brynhildrAPI(`/issue/${issueKey}`, {
-      method: 'GET',
-      headers: { 'Authorization': userAuthorization }
-    });
-    const newIssue = await newIssueResponse.json()
-    if (newIssue.key) {
-      const response = await brynhildrAPI(`/transition/${newIssue.key}`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json', 'Authorization': userAuthorization }
-      })
-      const { transitions } = await response.json();
+    const newIssueResponse = await getIssue(issueKey, userAuthorization);
+
+    if (newIssueResponse.key) {
+      const transitions = await getTransitions(newIssueResponse.key, userAuthorization);
       const toTodoTransition = transitions.find((transition: TransitionProps) => transition.to.id === JiraStatusesId.TO_DO);
+
       if (toTodoTransition) {
-        const res = useDoTransition(newIssue.key, RNCEpicTransitionsId.ACIONAR_QUALIDADE, userAuthorization);
-        if (Number(res.status) === HttpStatus.NO_CONTENT) {
+        const res = await doTransition(newIssueResponse.key, RNCEpicTransitionsId.ACIONAR_QUALIDADE, userAuthorization);
+
+        if (res.status === HttpStatus.NO_CONTENT) {
           const newColumns = columns.map((column) => {
             if (column.id === JiraStatusesId.TO_DO) {
               return {
                 ...column,
-                tasks: [...column.tasks, newIssue],
+                tasks: [...column.tasks, newIssueResponse],
               };
             }
             return column;
@@ -65,50 +66,6 @@ const useCreateManual = (projectKey: string) => {
       }
     }
   }
-
-  const handleSubmitForm = useCallback(async (data: FieldValues) => {
-    if (projectKey !== JiraCategories.QUALITY_REPORT || !user) return;
-
-    setIsSubmitting(true);
-
-    const _data = filterFieldsValues(data);
-    Object.assign(_data, { reporter: user.getName() });
-
-    try {
-      if (isQualityMember) {
-        const result = await createQualityReport({ ..._data, actions });
-        const issue = await result.json();
-        if (result.status === HttpStatus.CREATED) {
-          await qualityIssueInTodo(issue.content.key)
-          toast.success(`Tarefa ${issue.content.key} criada com sucesso`);
-          handleCloseDialog();
-        }
-      } else {
-        const result = await createQualityReport({ ..._data, actions });
-        const issue = await result.json();
-        if (result.status === HttpStatus.CREATED) {
-          toast.success(`Tarefa ${issue.content.key} criada com sucesso`);
-          handleCloseDialog();
-        }
-      }
-    } catch (error) {
-      toast.error("Erro ao criar tarefa. Por favor, tente novamente.");
-    } finally {
-      setIsSubmitting(false);
-      form.reset();
-    }
-  }, [projectKey, user, createQualityReport, actions, form]);
-
-  const onHandleSendFormValues: SubmitHandler<FieldValues> = useCallback(
-    (data) => {
-      if (!data[CustomFields.TIPO_RELATORIO.id] || !data.attachments || data.attachments.length === 0) {
-        toast.warning("Campos obrigatórios não preenchidos: Tipo de Relatório e Anexos");
-        return;
-      }
-      handleSubmitForm(data);
-    },
-    [handleSubmitForm]
-  );
 
   const handleCloseDialog = useCallback(() => {
     setOpenDialog(false);
@@ -131,6 +88,54 @@ const useCreateManual = (projectKey: string) => {
   const memoizedFieldsComponents = useMemo(
     () => DynamicForm({ key: projectKey, fieldsComponents }),
     [projectKey, fieldsComponents]
+  );
+
+  const { mutateAsync: createIssueFn } = useMutation({
+    mutationFn: createIssue,
+    onSuccess: async (data) => {
+      if (isQualityMember) {
+        await qualityIssueInTodo(data.key)
+        if (form.getValues().attachments.length > 0) {
+          await sendAttachments({ issueKey: data.key, files: form.getValues().attachments });
+        }
+        toast.success(`Tarefa ${data.key} criada com sucesso`);
+        handleCloseDialog();
+      } else {
+        toast.success(`Tarefa ${data.key} criada com sucesso`);
+        handleCloseDialog();
+      }
+    },
+    onError: (error) => {
+      toast.error(`Erro ao criar tarefa: ${error}`);
+      setIsSubmitting(false);
+    },
+  })
+
+  const handleSubmitForm = useCallback(async (data: FieldValues) => {
+    const _data = filterFieldsValues(data);
+
+    const _epicName = await generateEpicName();
+    if (_epicName) Object.assign(_data, { epicName: _epicName })
+    if (_epicName) Object.assign(_data, { reporter: user && user.getName() });
+    if (form.watch('attachments').length > 0) {
+      Object.assign(_data, { attachments: form.watch('attachments') });
+    }
+
+    const requestBody = await epicBody(_data);
+
+    await createIssueFn({ fields: requestBody, userAuthorization });
+  }, [projectKey, user, createQualityReport, actions, form]);
+
+
+  const onHandleSendFormValues: SubmitHandler<FieldValues> = useCallback(
+    (data) => {
+      if (!data[CustomFields.TIPO_RELATORIO.id] || !data.attachments || data.attachments.length === 0) {
+        toast.warning("Campos obrigatórios não preenchidos: Tipo de Relatório e Anexos");
+        return;
+      }
+      handleSubmitForm(data);
+    },
+    [handleSubmitForm]
   );
 
   return {
