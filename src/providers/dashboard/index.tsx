3d3389286ec84dcf48d8fdc1c97c1f870e64197c
Wesley Ximenes <wximenes60@gmail.com>
2024-11-19 09:49:00 -0300

    chore: Passado provider Brynhildr em volta da Dashboard.

--- src/providers/dashboard/index.tsx
+++ src/providers/dashboard/index.tsx
@@ -6,25 +6,28 @@ import { ActionsProvider } from "./providers/actions/actions-provider";
 import { CauseAnalysisProvider } from "./providers/cause-analysis/cause-analysis-provider";
 import { CommentsProvider } from "./providers/comments/comments-provider";
 import { NotificationProvider } from "./providers/notifications/notifications-provider";
+import { BrynhildrProvider } from "../brynhildr/brynhildr-data-provider";
 
 export default function DashboardProviders({ children }: { children: ReactNode }) {
   return (
-    <SocketProvider>
-      <KabanProvider>
-        <WorklogProvider>
-          <ActionsProvider>
-            <CauseAnalysisProvider>
-              <CommentsProvider>
-                <NotificationProvider>
-                  <div className="bg-green-3 w-screen h-screen">
-                    {children}
-                  </div>
-                </NotificationProvider>
-              </CommentsProvider>
-            </CauseAnalysisProvider>
-          </ActionsProvider>
-        </WorklogProvider>
-      </KabanProvider>
-    </SocketProvider>
+    <BrynhildrProvider>
+      <SocketProvider>
+        <KabanProvider>
+          <WorklogProvider>
+            <ActionsProvider>
+              <CauseAnalysisProvider>
+                <CommentsProvider>
+                  <NotificationProvider>
+                    <div className="bg-green-3 w-screen h-screen">
+                      {children}
+                    </div>
+                  </NotificationProvider>
+                </CommentsProvider>
+              </CauseAnalysisProvider>
+            </ActionsProvider>
+          </WorklogProvider>
+        </KabanProvider>
+      </SocketProvider>
+    </BrynhildrProvider>
   );
 }
