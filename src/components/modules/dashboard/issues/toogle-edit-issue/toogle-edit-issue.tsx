0a60d16bbaf5633cdb0bb3d8f1933d5ddeed36ab
Wesley Ximenes <wximenes60@gmail.com>
2024-12-02 14:30:12 -0300

    chore: Adicionada funcionalidade de aprovação e rejeição do épico dentro da tarefa para a diretoria

--- src/components/modules/dashboard/issues/toogle-edit-issue/toogle-edit-issue.tsx
+++ src/components/modules/dashboard/issues/toogle-edit-issue/toogle-edit-issue.tsx
@@ -2,14 +2,24 @@
 import { DynamicForm } from "@/components/dynamic-form/dynamic-form";
 import { useBuildForm } from "@/components/dynamic-form/hooks/use-build-form";
 import { Button } from "@/components/ui/button";
+import { CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Textarea } from "@/components/ui/textarea";
+import { UserDTO } from "@/dtos/responses/user-dto";
 import { useActions } from "@/hooks/actions/use-actions";
+import { useAuth } from "@/hooks/auth/use-auth";
 import { useBrynhildrData } from "@/hooks/brynhildr-data/brynhildr-data";
+import { itWorkedOut } from "@/hooks/kanban/triggers/epic/epic-transitions";
+import { handleDiretoriaAprovaRelatorio } from "@/hooks/kanban/triggers/epic/handlers/handle-diretoria-aprova-relatorio";
+import { handleDiretoriaRejeitaRelatorio } from "@/hooks/kanban/triggers/epic/handlers/handle-diretoria-rejeita-relatorio";
+import { isUserInGroup } from "@/lib/utils/utils";
 import { BrynhildrService } from "@/services/external/brynhildr-service/brynhildr-service";
 import { buildRncActionsIssuesBulk } from "@/shared/builds/build-actions-rnc-issues-bulk";
 import { buildUpdateIssueFields } from "@/shared/builds/build-update-issue-fields";
 import { CustomFields } from "@/shared/constants/jira/jira-custom-fields";
+import { jiraGroups } from "@/shared/constants/jira/jira-groups";
+import { RNCEpicTransitionsId } from "@/shared/enums/rnc-enums/rnc-epic-transitions-id";
 import { avoidDefaultDomBehavior } from "@/shared/functions/avoidDefaultDomBehavior";
 import { useMutation } from "@tanstack/react-query";
 import { parseCookies } from "nookies";
@@ -29,10 +39,11 @@ interface ToggleEditIssueProps {
 
 const _ToggleEditIssue = ({ children, issueKey, projectKey }: ToggleEditIssueProps, ref: React.Ref<HTMLDivElement>) => {
 
+  const { user } = useAuth()
   const { '@valkyrie:auth-token': token } = parseCookies();
   const userAuth = `Basic ${token}`;
 
-  const { createBulkIssues, updateIssue, sendAttachments } = brynhildrService
+  const { createBulkIssues, updateIssue, sendAttachments, sendComment } = brynhildrService
   const { useGetIssue } = useBrynhildrData()
   const { data: epicIssue } = useGetIssue(issueKey, userAuth)
 
@@ -44,8 +55,10 @@ const _ToggleEditIssue = ({ children, issueKey, projectKey }: ToggleEditIssuePro
     fields: epicIssue?.fields,
   });
 
+  const [comment, setComment] = useState<string>('');
   const [isEditable, setIsEditable] = useState<boolean>(false);
   const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  const [showComments, setShowComments] = useState<boolean>(false);
 
   const onHandleChangeContent = useCallback(() => {
     setEnabled((prevState) => !prevState);
@@ -128,6 +141,14 @@ const _ToggleEditIssue = ({ children, issueKey, projectKey }: ToggleEditIssuePro
     await updateIssueFn({ issueKey, fields: body, userAuthorization: userAuth });
   }, []);
 
+  const handleSendRejectComment = async (comment: string) => {
+    const res = await sendComment(issueKey, comment, userAuth);
+    if (res) {
+      await handleDiretoriaRejeitaRelatorio(user as UserDTO, userAuth, issueKey, RNCEpicTransitionsId.DIRETORIA_REJEITA, itWorkedOut)
+      toast.success('Comentário enviado e transição realizada com sucesso');
+    }
+  }
+
   return (
     <Dialog open={isDialogOpen} onOpenChange={handleDialogClose} >
       <DialogTrigger asChild>{children}</DialogTrigger>
@@ -163,6 +184,31 @@ const _ToggleEditIssue = ({ children, issueKey, projectKey }: ToggleEditIssuePro
                 {DynamicForm({ key: projectKey, fieldsComponents: editableFieldComponents })}
                 <DialogFooter>
                   <div className="space-x-2">
+                    {isUserInGroup(user, jiraGroups.directorship) &&
+                      <Fragment>
+                        <Button type="button"
+                          onClick={async () => {
+                            await handleDiretoriaAprovaRelatorio(
+                              user as UserDTO,
+                              userAuth,
+                              issueKey,
+                              RNCEpicTransitionsId.APROVAR_RELATORIO_EM_SEGUNDA_INSTANCIA,
+                              itWorkedOut
+                            )
+                            handleDialogClose(false)
+                          }}
+                          className="bg-green-500">
+                          Aprovar
+                        </Button>
+                        <Button
+                          type="button"
+                          onClick={() => setShowComments(true)}
+                          className="bg-red-500"
+                        >
+                          Rejeitar
+                        </Button>
+                      </Fragment>
+                    }
                     <Button
                       type="submit"
                       disabled={!isEditable || form.formState.isSubmitting}
@@ -179,9 +225,39 @@ const _ToggleEditIssue = ({ children, issueKey, projectKey }: ToggleEditIssuePro
             )}
             <ScrollBar orientation="vertical" />
           </ScrollArea>
+          <Dialog open={showComments} onOpenChange={setShowComments} >
+            <DialogContent className="min-w-[700px] w-full max-w-md mx-auto" >
+              <CardHeader>
+                <CardTitle className="whitespace-nowrap" >
+                  {`Envie a justificativa da rejeição para a tarefa ${issueKey}`}
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <Textarea
+                  rows={10}
+                  placeholder="Escreva o comentário aqui..."
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                  className="min-h-[100px]"
+                />
+              </CardContent>
+              <CardFooter>
+                <Button onClick={() => {
+                  handleSendRejectComment(comment)
+                  setShowComments(false)
+                  handleDialogClose(false)
+                }}
+                  className="ml-auto mr-[15px]"
+                  type="submit"
+                  disabled={!comment.trim()}>
+                  Enviar justificativa e rejeitar
+                </Button>
+              </CardFooter>
+            </DialogContent>
+          </Dialog>
         </DialogContent>
       </FormProvider>
-    </Dialog>
+    </Dialog >
   )
 }
 
